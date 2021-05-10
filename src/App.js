6800fded8c3710cdabde05e65cc64ff7fd13b8b9
milanm98 <marjanovmilan13@gmail.com>
2021-05-10 12:19:51 +0200

    fields

--- src/App.js
+++ src/App.js
@@ -1,17 +1,122 @@
-import logo from './logo.svg';
 import './App.css';
+import { buildStyles, CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const[percentValue, setPercentValue] = useState(0);
+  const[temp, setTemp] = useState("%");
+  
+  const[addMoreUsername, setAddMoreUsername] = useState(true);
+  const[addMoreEmail, setAddMoreEmail] = useState(true);
+  const[addMorePassword, setAddMorePassword] = useState(true);
+  const[addMoreRepeatPassword, setAddMoreRepeatPassword] = useState(true);
+  
+
+  const handleUsernameChange = (e) => {
+    if(e.target.value !== "" && addMoreUsername){
+      
+      setPercentValue(percentValue + 25);
+      setAddMoreUsername(false);
+    }
+    else if(e.target.value === ""){
+      setPercentValue(percentValue - 25);
+      setAddMoreUsername(true);
+    }
+
+    if(percentValue === 100){
+      setTemp("");
+      setPercentValue("PROCEED")
+    }
+  }
+
+  const handleEmailChange = (e) => {
+    if(e.target.value !== "" && addMoreEmail){
+      
+      setPercentValue(percentValue + 25);
+      setAddMoreEmail(false);
+    }
+    else if(e.target.value === ""){
+      setPercentValue(percentValue - 25);
+      setAddMoreEmail(true);
+    }
+
+    if(percentValue === 100){
+      setTemp("");
+      setPercentValue("PROCEED")
+    }
+
+  }
+
+  const handlePasswordChange = (e) => {
+    if(e.target.value !== "" && addMoreRepeatPassword){
+      
+      setPercentValue(percentValue + 25);
+      setAddMoreRepeatPassword(false);
+    }
+    else if(e.target.value === ""){
+      setPercentValue(percentValue - 25);
+      setAddMoreRepeatPassword(true);
+    }
+
+    if(percentValue === 100){
+      setTemp("");
+      setPercentValue("PROCEED")
+    }
+  }
+
+  const handlePasswordRepeatChange = (e) => {
+    if(e.target.value !== "" && addMorePassword){
+      
+      setPercentValue(percentValue + 25);
+      setAddMorePassword(false);
+    }
+    else if(e.target.value === ""){
+      setPercentValue(percentValue - 25);
+      setAddMorePassword(true);
+    }
+
+    if(percentValue === 100){
+      setTemp("");
+      setPercentValue("PROCEED")
+    }
+
+  }
+
+  const handleClick = (e) => {
+    if(percentValue === "PROCEED") {
+      alert("You are now registered, welcome ! ");
+    }
+  }
+
   return (
     <div className="tracking-widest leading-loose font-mono font-black text-2xl text-center w-screen h-screen bg-gray-200">
       <h1 className="border border-green-600 border-b-8 w-full h-auto text-green-600">REGISTRATION</h1>
       <form className="w-full my-12">
-        <input type="text" placeholder="username" className="border-2 border-green-600 my-6 text-center"></input>
-        <input type="text" placeholder="email" className="border-2 border-green-600 my-6 text-center"></input>
-        <input type="text" placeholder="password" className="border-2 border-green-600 my-6 text-center"></input>
-        <input type="text" placeholder="repeat password" className="border-2 border-green-600 my-6 text-center"></input>
+        <input onChange={handleUsernameChange} type="text" placeholder="username" className="h-11 border-4 border-green-600 my-6 text-center"></input>
+        <input onChange={handleEmailChange} type="email" placeholder="email" className="h-11 border-4 border-green-600 my-6 text-center"></input>
+        <input onChange={handlePasswordChange} type="password" placeholder="password" className="h-11 border-4 border-green-600 my-6 text-center"></input>
+        <input onChange={handlePasswordRepeatChange} type="password" placeholder="repeat password" className="h-11 border-4 border-green-600 my-6 text-center"></input>
       </form>
-
+      
+      <div className="flex text-center items-center justify-center">
+        <div style={{ width : 200, height : 200, textAlign:'center'}}>
+            <CircularProgressbarWithChildren styles={buildStyles({
+              pathColor: "#059669",
+              textColor: "#059669",
+            })} value={percentValue}>
+            <button onClick={handleClick}>
+              <div className="flex text-green-600 font-black text-4xl font-sans">
+                <span>{percentValue}</span>
+                <span>{temp}</span>
+              </div>
+            </button>
+              </CircularProgressbarWithChildren>
+            
+        </div>
+      </div>
     </div>
   );
 }
